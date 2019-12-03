6cc99a0527b9922eff2e59351cdaefae559b2e6a
ykalashn <ykalashn@student.hive.fi>
2019-12-03 18:03:34 +0200

    all

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykalashn <ykalashn@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/15 12:34:09 by ykalashn          #+#    #+#             */
+/*   Updated: 2019/12/03 17:16:14 by ykalashn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 3
+# define CEILING 2048
+# include "libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
