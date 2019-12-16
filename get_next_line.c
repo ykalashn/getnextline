384c71795e38906ba768fb832497f23a9d1fa7ce
ykalashn <ykalashn@student.hive.fi>
2019-12-16 14:32:09 +0200

    update finished getnextline before test

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,54 @@
 /*   By: ykalashn <ykalashn@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 12:33:55 by ykalashn          #+#    #+#             */
-/*   Updated: 2019/12/03 14:26:14 by ykalashn         ###   ########.fr       */
+/*   Updated: 2019/12/16 14:30:43 by ykalashn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		get_next_line(const int fd, char **line)
+#include "get_next_line.h"
+
+static int	ft_new_line(char *rubbish, char **line)
 {
-	fd = read 
+	int len;
+
+	len = 0;
+	while (rubbish[len] != '\0' && rubbish[len] != '\n')
+		len++;
+	if (!(*line = ft_strndup(rubbish, len)))
+		return (-1);
+	if (rubbish[len] == '\n')
+		rubbish = ft_strcpy(rubbish, &rubbish[len + 1]);
+	else
+		rubbish[0] = '\0';
+	return (1);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	static char	*rubbish[CEILING];
+	char		buf[BUFF_SIZE + 1];
+	char		*tmp;
+	int			ret;
+
+	i = 0;
+	ret = 1;
+	if (fd < 0 || line == NULL)
+		return (-1);
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	{
+		buf[ret] = '\0';
+		if (rubbish[fd] == NULL)
+			rubbish[fd] = ft_strnew(0);
+		tmp = rubbish[fd];
+		if (!(rubbish[fd] = ft_strjoin(rubbish[fd], buf)))
+			return (-1);
+		ft_strdel(&tmp);
+		if (ft_strchr('\n'))
+			break ;
+	}
+	if (ret < 0)
+		return (-1);
+	else if (ret == 0 && (rubbish[fd] == NULL || rubbish[fd][0] == '\0'))
+		return (0);
+	return (ft_new_line(rubbish[fd], line));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: ykalashn <ykalashn@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 12:34:09 by ykalashn          #+#    #+#             */
-/*   Updated: 2019/12/03 17:16:14 by ykalashn         ###   ########.fr       */
+/*   Updated: 2019/12/10 15:14:56 by ykalashn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 3
-# define CEILING 2048
+# define CEILING 4864
 # include "libft.h"
 
 int		get_next_line(const int fd, char **line);
